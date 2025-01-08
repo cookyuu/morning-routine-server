d3119313916bd38effb11659a46ddea2e6eaebd1
임해규 <limk9500@gmail.com>
2025-01-08 12:44:59 +0900

    feature(weather):  Weather Entity 수정

---
+++ src/main/java/com/cookyuu/morning_routine/domain/weather/dto/WeatherDivideByTimeDto.java
@@ -0,0 +1,16 @@
+package com.cookyuu.morning_routine.domain.weather.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CollectWeatherDataDivideByTimeDto {
+    private LocalDate
+}
